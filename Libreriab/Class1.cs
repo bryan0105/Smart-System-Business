bb60123a3cde9635d880c80ca14bc0b6562f5b04
Bryan Torres <bryantorres0105@gmail.com>
2017-06-01 08:25:35 -0400

    agregar archivo txt nuevo

---
+++ Libreriab/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Libreriab
+{
+    public class Utileria 
+    {
+        public static DataSet Ejecutar(string cmd)
+        {
+            SqlConnection ConnetiongString = new SqlConnection("Data Source=.;Initial Catalog=saewin;Integrated Security=True");
+            ConnetiongString.Open();
+
+            DataSet DS = new DataSet();
+            SqlDataAdapter DP = new SqlDataAdapter(cmd,ConnetiongString);
+
+            DP.Fill(DS);
+
+            ConnetiongString.Close();
+
+            return DS;
+        }
+    }
+}
